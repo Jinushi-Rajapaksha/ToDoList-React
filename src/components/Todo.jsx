80657b789bd3556b6ce79746cf19a0ddb9289af1
Jinushi-Rajapaksha <jinushi.20234017@iit.ac.lk>
2023-12-26 22:16:05 +0530

    add icons to todoitems

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useRef } from "react";
 import { useEffect } from "react";
 import "./css/Todo.css";
+import Todoitems from "./Todoitems.jsx";
 
 let count = 0;
 const Todo = () => {
@@ -27,7 +28,12 @@ const Todo = () => {
             <div  onClick={()=>{add()}} className="todo-add-btn">ADD</div>
         </div>
 
-      <div className="Todo-list"></div>
+      <div className="Todo-list">
+        {todos.map((item,index)=>{
+          return <Todoitems key={index} no={item.no} display={item.display} text={item.text}/>
+
+        })}
+      </div>
     </div>
   );
 };
--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -1,6 +1,20 @@
 import './css/Todoitems.css'
-export const Todoitems = () => {
+import right from './assets/right.png';
+import circle from './assets/circle.jpg';
+import wrong from './assets/wrong.png';
+
+ const Todoitems = ({no,display,text}) => {
   return (
-    <div>Todoitems</div>
+    <div className='Todoitems'>
+      <div className="todoitems">
+      <img src={wrong}  alt="" />
+      <img src={right}  alt="" />
+      <div className="todoitems-text">{text}</div>
+      </div>
+      <img className='todoitems-cross-icon'src={circle}  alt="" />
+    </div>
+   
   )
 }
+
+export default Todoitems
